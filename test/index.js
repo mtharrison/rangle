a7061626d2e028d88deda350e663a4a05eecb172
Matt Harrison <hi@matt-harrison.com>
2016-09-26 17:08:46 +0100

    Remove any empty chunks immediately

--- test/index.js
+++ test/index.js
@@ -59,6 +59,52 @@ describe('Rangle', () => {
         done();
     });
 
+    it('accepts a path with a .', (done) => {
+
+        const ranges = ['0-10:10', '10-20:2', '20-30:3', '30-40:4'];
+
+        const items = {
+            1: { modified: { ts: 1 } },
+            2: { modified: { ts: 25 } },
+            3: { modified: { ts: 31 } },
+            4: { modified: { ts: 38 } },
+            5: { modified: { ts: 1 } },
+            6: { modified: { ts: 28 } },
+            7: { modified: { ts: 8 } },
+            8: { modified: { ts: 24 } },
+            9: { modified: { ts: 8 } },
+            10: { modified: { ts: 20 } }
+        };
+
+        const newRanges = Rangle.ranges(items, ranges, { path: 'modified.ts' });
+
+        expect(newRanges).to.equal(['0-10', '10-20', '20-30', '30-40']);
+        done();
+    });
+
+    it('throws on path with more than 1 .', (done) => {
+
+        const ranges = ['0-10:10', '10-20:2', '20-30:3', '30-40:4'];
+
+        const items = {
+            1: { modified: { ts: 1 } },
+            2: { modified: { ts: 25 } },
+            3: { modified: { ts: 31 } },
+            4: { modified: { ts: 38 } },
+            5: { modified: { ts: 1 } },
+            6: { modified: { ts: 28 } },
+            7: { modified: { ts: 8 } },
+            8: { modified: { ts: 24 } },
+            9: { modified: { ts: 8 } },
+            10: { modified: { ts: 20 } }
+        };
+
+        const fn = () => Rangle.ranges(items, ranges, { path: 'modified.ts.other' });
+
+        expect(fn).to.throw('Doesn\'t support paths with more than 1 .');
+        done();
+    });
+
     it('should add a new range when below limit of ranges', (done) => {
 
         const ranges = ['0-10:2', '10-20:2'];
@@ -132,6 +178,66 @@ describe('Rangle', () => {
         const newRanges = Rangle.ranges(items, ranges);
 
         expect(newRanges).to.equal(['0-10', '10-20', '20-40', '40-50', '50-52']);
+        done();
+    });
+
+    it('should evict any chunks with zero valid items', (done) => {
+
+        const ranges = [
+            '0-10:2',
+            '10-20:4',
+            '20-30:3',  // 0 items here
+            '30-40:3',
+            '40-50:2'
+        ];
+
+        const items = {
+            1: { modified: 1 },
+            2: { modified: 1 },
+            3: { modified: 11 },
+            4: { modified: 18 },
+            5: { modified: 19 },
+            6: { modified: 19 },
+            7: { modified: 31 },
+            8: { modified: 32 },
+            9: { modified: 34 },
+            10: { modified: 42 },
+            11: { modified: 48 }
+        };
+
+        const newRanges = Rangle.ranges(items, ranges);
+
+        expect(newRanges).to.equal(['0-10', '10-20', '30-40', '40-50']);
+        done();
+    });
+
+    it('should evict multiple chunks with zero valid items', (done) => {
+
+        const ranges = [
+            '0-10:2',
+            '10-20:4',
+            '20-30:3',  // 0 items here
+            '30-40:3',  // 0 items here
+            '40-50:5'
+        ];
+
+        const items = {
+            1: { modified: 1 },
+            2: { modified: 1 },
+            3: { modified: 11 },
+            4: { modified: 18 },
+            5: { modified: 19 },
+            6: { modified: 19 },
+            7: { modified: 41 },
+            8: { modified: 42 },
+            9: { modified: 44 },
+            10: { modified: 42 },
+            11: { modified: 48 }
+        };
+
+        const newRanges = Rangle.ranges(items, ranges);
+
+        expect(newRanges).to.equal(['0-10', '10-20', '40-50']);
         done();
     });
 
